268c8c702d5631585b83a9c88b89eb8f02836a32
Filip Sedlisky <sedlifil@fel.cvut.cz>
2018-12-09 12:23:22 +0100

    add workerRest methods

--- src/main/java/cz/cvut/kbss/ear/brigade/rest/WorkerController.java
+++ src/main/java/cz/cvut/kbss/ear/brigade/rest/WorkerController.java
@@ -1,9 +1,12 @@
 package cz.cvut.kbss.ear.brigade.rest;
 
 import cz.cvut.kbss.ear.brigade.exception.NotFoundException;
+import cz.cvut.kbss.ear.brigade.model.Brigade;
 import cz.cvut.kbss.ear.brigade.model.Worker;
 import cz.cvut.kbss.ear.brigade.rest.util.RestUtils;
+import cz.cvut.kbss.ear.brigade.service.BrigadeService;
 import cz.cvut.kbss.ear.brigade.service.WorkerService;
+import javafx.util.Pair;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +16,8 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/workers")
 public class WorkerController {
@@ -20,10 +25,12 @@ public class WorkerController {
     private static final Logger LOG = LoggerFactory.getLogger(WorkerController.class);
 
     private final WorkerService workerService;
+    private final BrigadeService brigadeService;
 
     @Autowired
-    public WorkerController(WorkerService workerService) {
+    public WorkerController(WorkerService workerService, BrigadeService brigadeService) {
         this.workerService = workerService;
+        this.brigadeService = brigadeService;
     }
 
     @RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
@@ -35,6 +42,14 @@ public class WorkerController {
     }
 
 
+    @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Worker> getWorkers() {
+        final List<Worker> workers = workerService.findAll();
+        if (workers == null) {
+            throw NotFoundException.create("Workers", "findAll()");
+        }
+        return workers;
+    }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public Worker getWorker(@PathVariable("id") Integer id) {
@@ -45,4 +60,90 @@ public class WorkerController {
         return worker;
     }
 
+    @RequestMapping(value = "/{id}/brigades/future", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Brigade> getFutureBrigades(@PathVariable("id") Integer id) {
+        Worker worker = workerService.find(id);
+        if (worker == null) {
+            throw NotFoundException.create("Worker", id);
+        }
+        return workerService.getFutureBrigades(worker);
+    }
+
+    @RequestMapping(value = "/{id}/brigades/past", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Brigade> getPastBrigades(@PathVariable("id") Integer id) {
+        Worker worker = workerService.find(id);
+        if (worker == null) {
+            throw NotFoundException.create("Worker", id);
+        }
+        return workerService.getPastBrigades(worker);
+    }
+
+    @RequestMapping(value = "/signOff/brigade/{workerId}/{brigadeId}", method = RequestMethod.DELETE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void signOffFromBrigade(@PathVariable("workerId") Integer workerId, @PathVariable("brigadeId") Integer brigadeId) {
+        Worker worker = workerService.find(workerId);
+        if (worker == null) {
+            throw NotFoundException.create("Worker", workerId);
+        }
+        Brigade brigade = brigadeService.find(brigadeId);
+        if (brigade == null) {
+            throw NotFoundException.create("Brigade", brigadeId);
+        }
+        workerService.singOffFromBrigade(worker, brigade);
+    }
+
+    @RequestMapping(value = "/add/thumbsUp/brigade/{workerId}/{brigadeId}", method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void addThumbsUpBrigade(@PathVariable("workerId") Integer workerId, @PathVariable("brigadeId") Integer brigadeId) {
+        Worker worker = workerService.find(workerId);
+        if (worker == null) {
+            throw NotFoundException.create("Worker", workerId);
+        }
+        Brigade brigade = brigadeService.find(brigadeId);
+        if (brigade == null) {
+            throw NotFoundException.create("Brigade", brigadeId);
+        }
+
+        workerService.addThumbsUpToBrigade(worker, brigade);
+    }
+
+    @RequestMapping(value = "/add/thumbsDown/brigade/{workerId}/{brigadeId}", method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void addThumbsDownBrigade(@PathVariable("workerId") Integer workerId, @PathVariable("brigadeId") Integer brigadeId) {
+        Worker worker = workerService.find(workerId);
+        if (worker == null) {
+            throw NotFoundException.create("Worker", workerId);
+        }
+        Brigade brigade = brigadeService.find(brigadeId);
+        if (brigade == null) {
+            throw NotFoundException.create("Brigade", brigadeId);
+        }
+
+        workerService.addThumbsDownToBrigade(worker, brigade);
+    }
+
+    @RequestMapping(value = "/{id}/score", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public Pair<Integer, Integer> getWorkerScore(@PathVariable("id") Integer id) {
+        Worker worker = workerService.find(id);
+        if (worker == null) {
+            throw NotFoundException.create("Worker", id);
+        }
+        return workerService.getWorkerScore(worker);
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
